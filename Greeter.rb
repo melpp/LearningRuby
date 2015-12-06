25ed605589ab4923b890a39a9ce8efbf6f86b0d4
melpp <melpp@users.noreply.github.com>
2015-12-05 23:28:53 -0800

    Update Greeter.rb

--- Greeter.rb
+++ Greeter.rb
@@ -14,8 +14,6 @@ greeter = Greeter.new ("sabriye")
 greeter.say_hi
 greeter.say_bye
 
-Greeter.instance_methods
-
 print "To findout about other methods exist for Greeter objects => Greeter.instance_methods \n "
 
 print "Well this will give us all of the methods for Greeter objects, a complete list, including ones defined by ancestor classes. If we want to just list methods defined for Greeter we can tell it to not include ancestors by passing it the parameter false, meaning we don’t want methods defined by ancestors => Greeter.instance_methods(false) \n"
